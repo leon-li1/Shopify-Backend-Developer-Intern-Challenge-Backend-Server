f5dc06b6ca5f10d4c5effb3496235b49f38d1044
Leon Li <leonli369@gmail.com>
2022-01-16 02:31:03 -0500

    changed cors

--- app/main.py
+++ app/main.py
@@ -6,10 +6,9 @@ from .routers import items
 
 app = FastAPI()
 
-origins = ['https://shopify-backend-developer-intern-challenge-e17dv1pnc-leon-li1.vercel.app']
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
